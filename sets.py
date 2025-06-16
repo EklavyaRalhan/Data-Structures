0cbe0e93b596443b6ccfccc6fe1e3550cf6b8ab5
EklavyaRalhan <122105141+EklavyaRalhan@users.noreply.github.com>
2025-06-16 18:50:51 +0400

    Add files via upload
    Operations on sets

---
+++ sets.py
@@ -0,0 +1,13 @@
+integers = {3,6,9,12}
+print(integers,"\n")
+
+mixed = {"bird",5,"fly",8}
+print(mixed,"\n")
+
+elements = {1,2,3,4,3,2,4}
+print(elements,"\n")
+
+userelement = set([1,2,3,4])
+print(userelement,"\n")
+
+print(userelement.pop())
