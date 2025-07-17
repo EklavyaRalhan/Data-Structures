4e935f0cdba5db20fb98468d781b024f5bba862f
EklavyaRalhan <122105141+EklavyaRalhan@users.noreply.github.com>
2025-07-17 19:13:25 +0400

    Add files via upload

---
+++ coordinates.py
@@ -0,0 +1,13 @@
+class Cooardinates:
+    def __init__(self,x=0,y=0):
+         self.x = x
+         self.y = y
+
+    def printcoordinates(self):
+         print("Coordinates are","(", self.x,self.y,")")
+
+coordinates = Cooardinates(76,78)
+
+coordinates.printcoordinates()
+
+        
