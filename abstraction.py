22dc4f593ecaf51fff20edad2de714848c920ebd
EklavyaRalhan <122105141+EklavyaRalhan@users.noreply.github.com>
2025-07-28 18:49:54 +0400

    Add files via upload

---
+++ abstraction.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+class absclass(ABC):
+
+    def print(self,x):
+        print("Value is ", x)
+
+
+    @abstractmethod
+    def task(self):
+        print("We are inside absclass")
+
+class test_class(absclass):
+
+    def task(self):
+     print("We are inside test_class")
+
+test_obj = test_class()
+test_obj.task()
+test_obj.print(100)
